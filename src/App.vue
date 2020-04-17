844d5a21fcf97d73cc166ae664ff3b3a21aee58f
Matt Bidewell <matthewbidewell@gmail.com>
2020-04-17 18:24:43 +0100

    WIP light theme

--- src/App.vue
+++ src/App.vue
@@ -23,20 +23,39 @@ export default {
 
   @import url('https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap');
 
+  :root {
+    --font-color: #d6d6d6;
+    --font-color-dark:#16191b;
+    --background-color: #16191b;
+    --background-color-light: #e7e7e7;
+  }
+
+  .dark {
+    --bg: var(--background-color);
+    --font: var(--font-color);
+  }
+
+  .light {
+    --bg: var(--background-color-light);
+    --font: var(--font-color-dark);
+  }
+
   body {
     width: 100%;
     height: 100%;
-    background: #1b1b1b;
     color: white;
     text-align:left;
     font-family: 'Roboto Mono', monospace;
     overflow-x: hidden;
+    background: var(--bg);
   }
 
   #app {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
+    color: #030303;
+    width: 100%;
+    height: 100%;
     /* margin-top: 60px; */
   }
 
@@ -45,4 +64,5 @@ export default {
     margin-left:20%;
     margin-right:20%;
   }
+
 </style>
