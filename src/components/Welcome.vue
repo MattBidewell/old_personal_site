ac1e0c2a4bd1fe034cae386f1d5247a577179640
Matt Bidewell <matthewbidewell@gmail.com>
2020-12-21 18:57:52 +0000

    style update

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="welcome">
     <h1 class="crypto">I'm Matthew Bidewell ICON</h1>
-    <p class= "crypto">I'm a Software engineer 👨‍💻 with yearsHOLD years of experience in creating well crafted SAAS solutions. I'm part of the team over at A_Million_Ads where I plan, build and reflect.</p> <br/>
-    <p class= "crypto">When I'm not programming, my distractions include cinematography🎥, cyber security and browsing record stores🎧.</p> <br/>
+    <br/>
+    <p class= "crypto">I'm a Software engineer with yearsHOLD years of experience in creating well crafted SAAS solutions using AWS. I'm part of the team over at A_Million_Ads where I help build high performing, low latancy services.</p> <br/>
+    <p class= "crypto">When I'm not programming, my distractions include drone cinematography 🎥, cyber security and browsing record stores 🎧.</p> <br/>
     <p class= "crypto">You can find me on Twitter, scrutinise my code at Github or get in touch via LinkedIn.</p><br/>
   </div>
 </template>
@@ -35,21 +36,21 @@
               } else if(word === "A_Million_Ads") {
                   const letters = word.split("_");
                   const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join(" "); // space becuase multiple words
-                  return `<a href="https://www.amillionads.com/">${text}</a>`;
+                  return `<a id="amillionads" href="https://www.amillionads.com/">${text}</a>`;
               } else if(word === "Twitter,") {
                   const letters = word.split("");
                   const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
-                  return `<a href="https://twitter.com/MatthewDev_">${text}</a>`
+                  return `<a id="twitter" href="https://twitter.com/MatthewDev_">${text}</a>`
               } else if(word === "Github") {
                   const letters = word.split("");
                   console.log(letters);
                   const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
-                  return `<a href="https://github.com/MattBidewell">${text}</a>`;
+                  return `<a id="github" href="https://github.com/MattBidewell">${text}</a>`;
               } else if(word === "LinkedIn."){
                 const letters = word.split("");
                   console.log(letters);
                   const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
-                  return `<a href="https://www.linkedin.com/in/matt-bidewell-a4578790/">${text}</a>`;
+                  return `<a id="linkedin" href="https://www.linkedin.com/in/matt-bidewell-a4578790/">${text}</a>`;
               } else {
                   const letters = word.split("");
                   return letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
@@ -97,4 +98,29 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 
+
+    #amillionads *, #amillionads *:hover {
+        color: #f77f26;
+    }
+    
+    #amillionads:link, #linkedin:link, #twitter:link, #github:link {
+        text-decoration: none;
+    }
+
+    #amillionads *:hover, #linkedin:hover, #twitter:hover, #github:hover {
+        background-color: transparent;
+        text-decoration: underline;
+    }
+
+    #linkedin *, #linkedin:hover, #linkedin:visited{
+        color: #0077B5; /* linkedIn blue */
+    }
+
+    #github *, #github:hover, #github:visited{
+        color:#6a737d; /* github blue?*/
+    }
+
+    #twitter *, #twitter:hover, #twitter:visited{
+        color: #1DA1F2; /* Twitter blue */
+    }
 </style>
