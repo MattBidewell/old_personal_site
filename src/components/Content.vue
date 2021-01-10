f321d58b3daf96b1f8d59e62e94a10067d46cc5c
Matt Bidewell <matthewbidewell@gmail.com>
2021-01-10 20:16:19 +0000

    added blog posts. Updated styling. Added new links.

---
+++ src/components/Content.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <nav id="header">
+        <p class="crypto">LinkedInLink</p>
+        <p class="crypto">GithubLink</p>
+        <p class="crypto">TwitterLink</p>
+        <p class="crypto">MediumLink</p>
+    </nav>
+    <section id="content">
+        <h1 class="crypto">I'm Matthew Bidewell ICON</h1>
+        <br/>
+        <p class= "crypto">I'm a Software engineer with yearsHOLD years of experience in creating well crafted SAAS solutions using AWS. I'm part of the team over at A_Million_Ads where I help build highly scalable, low latancy services. 🐝</p> <br/>
+        <p class= "crypto">When I'm not programming, my distractions include drone cinematography 🎥, cyber security and browsing record stores 🎧.</p> <br/>
+        <p class= "crypto">You can find me on Twitter, scrutinise my code at Github or get in touch via LinkedIn.</p><br/>
+        <section id="blog">
+            <h2 class="crypto">Blog</h2>
+            <div class = "post">
+                <a class="post_link" href="https://medium.com/@matthewbidewell/rust-and-its-ownership-model-effc41e3951">
+                    <h3 class="crypto">Rust and ownership</h3>
+                    <p class="crypto">A high level introduction to Rust and its owership rules. The post talks about heap, stack and ownership.</p>
+                </a>
+            </div>
+        </section>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Content',
+    mounted: function() {
+
+      const chars = "ABCDEFGHIJKLMNOPQRSTUVWYZXabcdefghijklmnopqrstuvwyzx1234567890-=!£$%^&*()_+`¬_+[];'#,./<>?:@~{}Ü¥";
+      const emojis = ["💀","☠️","💩","🤡","👹","👺","👻","👽","👾","🤖","👋","🤏","🖖","🤘","🤟","🧠","👨‍🎤","🕵️","📷","🐕"];
+
+      const parentElements = this.$el.getElementsByClassName("crypto");
+
+      for(const element of parentElements) {
+          const words = element.innerHTML.split(" ");
+          const newWords = words.map((word)=> {
+
+              if(word==="ICON"){
+                  const random = Math.floor(Math.random() * Math.floor(emojis.length));
+                  word = emojis[random];
+                  const letters = word.split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return text;
+              } else if(word === "yearsHOLD") {
+                  word = `${new Date().getFullYear() - 2015}+`;
+                  const letters = word.split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return text;
+              } else if(word === "A_Million_Ads") {
+                  const letters = word.split("_");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join(" "); // space becuase multiple words
+                  return `<a id="amillionads" href="https://www.amillionads.com/">${text}</a>`;
+              } else if(word === "Twitter,") {
+                  const letters = word.split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a id="twitter" href="https://twitter.com/MatthewDev_">${text}</a>`
+              } else if(word === "Github") {
+                  const letters = word.split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a id="github" href="https://github.com/MattBidewell">${text}</a>`;
+              } else if(word === "LinkedIn."){
+                  const letters = word.split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a id="linkedin" href="https://www.linkedin.com/in/matt-bidewell-a4578790/">${text}</a>`;
+              } else if(word === "LinkedInLink"){
+                  const letters = "Linkedin".split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a class="base_link" href="https://www.linkedin.com/in/matt-bidewell-a4578790/">${text}</a>`;
+              } else if(word === "GithubLink"){
+                  const letters = "Github".split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a class="base_link" href="https://github.com/MattBidewell">${text}</a>`;
+              } else if(word === "TwitterLink"){
+                  const letters = "Twitter".split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a class="base_link" href="https://twitter.com/MatthewDev_">${text}</a>`;
+              } else if(word === "MediumLink"){
+                  const letters = "Medium".split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a class="base_link" href="https://medium.com/@matthewbidewell">${text}</a>`;
+              } else if(word === "RustOwnershipLink") {
+                  const letters = "Read more..".split("");
+                  const text = letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+                  return `<a class="base_link" href="https://medium.com/@matthewbidewell/rust-and-its-ownership-model-effc41e3951">${text}</a>`;
+              }else {
+                  const letters = word.split("");
+                  return letters.map((letter)=>`<span class="cp">${letter}</span>`).join("");
+              }
+          });
+          element.innerHTML = newWords.join(" ");
+      };
+
+      const cryptoElements = this.$el.getElementsByClassName("cp");
+      
+      for(let i = 0; i < cryptoElements.length; i++) {
+          const element = cryptoElements[i];
+          const originalText = element.textContent;
+          const cryptoText = getCryptoWord(originalText.length);
+          element.innerHTML = cryptoText;
+          setOrignal(element,originalText);
+      }
+
+      function getCryptoWord(length) {
+          const word = [];
+          while(length > 0) {
+              const random = Math.floor(Math.random() * Math.floor(chars.length));
+              word.push(chars[random]);
+              length = length - 1;
+          }
+          return word.join("");
+      }
+
+      function setOrignal(element, originalText) {
+        const time = Math.random() * Math.floor(3000);
+        setTimeout((element, originalText) => {
+            element.innerHTML = originalText;
+            element.className = "crypto-post";
+        }, time, element, originalText);  
+      };
+
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+    h1 {
+        font-size: 1.3em;
+    }
+    
+    h2 {
+        font-size: 1.2em;
+    }
+
+    #content {
+        margin-left:15rem;
+        margin-right:15rem;
+    }
+
+    #header {
+        text-align: right;
+        padding:1rem;
+        flex:1;
+    }
+
+    #header p {
+        display: inline;
+    }
+
+    #header p a{
+        text-decoration: none;
+        padding: 0 25px;
+    }
+
+    #header p a:link, #header p a:visited, #header p a:active, #haeder *{
+        color:var(--font-link);
+    }
+
+    #header a:hover, .base_link:hover {
+        text-decoration:underline;
+        color:var(--link-hover)
+    }
+
+    #amillionads *, #amillionads *:hover {
+        color: #f77f26;
+    }
+    
+    #amillionads:link, #linkedin:link, #twitter:link, #github:link {
+        text-decoration: none;
+    }
+
+    #amillionads *:hover, #linkedin:hover, #twitter:hover, #github:hover {
+        background-color: transparent;
+        text-decoration: underline;
+    }
+
+    #linkedin *, #linkedin:hover, #linkedin:visited{
+        color: #0077B5; /* linkedIn blue */
+    }
+
+    #github *, #github:hover, #github:visited{
+        color:#6a737d; /* github blue?*/
+    }
+
+    #twitter *, #twitter:hover, #twitter:visited{
+        color: #1DA1F2; /* Twitter blue */
+    }
+
+    .base_link, .post_link{
+        text-decoration: none;
+    }
+
+    .base_link *, .base_link:hover, .base_link:visited, .post_link {
+        color:var(--font-link);
+    }
+
+    .post {
+        margin-left: 30px;
+        padding-left: 10px;
+        border-left: 2px solid var(--font-link);
+    }
+
+    .post:hover {
+        border-bottom: 2px solid var(--font-link);
+    }
+</style>
